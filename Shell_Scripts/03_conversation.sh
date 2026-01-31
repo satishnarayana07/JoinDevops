800d1ad60dc718e74aadfc42ca1c71a43565fe45
Satish Narayana <satishnarayana07@gmail.com>
2026-01-31 08:09:06 +0530

    practie

---
+++ Shell_Scripts/03_conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1:: hi $person2, how are you?"
+echo "$person2:: I am fine $person1, good to see you"
+echo "$person1:: really i am glad to see you $person2"
+echo "$person2:: hmm..happ to me also,how about your family $person1?"
