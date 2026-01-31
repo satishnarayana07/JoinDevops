39827b5271a124cb8b1a7e2cd449cbeb8dc01e02
Satish Narayana <satishnarayana07@gmail.com>
2026-01-31 08:17:43 +0530

    practie

--- Shell_Scripts/04_variable.sh
+++ Shell_Scripts/04_variable.sh
@@ -6,3 +6,6 @@ echo "here is your name buddy: $username"
 echo "Dear $username,pleae enter your mobile number:"
 read mobilenumber
 echo "here is your mobile number : $mobilenumber"
+echo "Dear $username, please enter your password:"
+read -s password
+echo " your password is : $password"
