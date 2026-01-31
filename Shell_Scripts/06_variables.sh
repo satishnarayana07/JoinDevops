4e5e83ee0573637a8eed86a9a466d90c513b9781
Satish Narayana <satishnarayana07@gmail.com>
2026-01-31 08:44:18 +0530

    practie

--- Shell_Scripts/06_variables.sh
+++ Shell_Scripts/06_variables.sh
@@ -3,7 +3,7 @@
 echo "all args passed to the script: $@"
 echo "number of variables passed to the script: $#"
 echo "script name: $0"
-echo "present working directory: $pwd"
+echo "present working directory: $PWD"
 echo " who is running script: $USER"
 echo "home directory of current user: $HOME"
 echo "PID of the script: $$"
