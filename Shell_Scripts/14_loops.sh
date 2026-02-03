659057aab60327f2a859216f6e2d99f3244f57d8
Satish Narayana <satishnarayana07@gmail.com>
2026-02-03 21:26:11 +0530

    practice

---
+++ Shell_Scripts/14_loops.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+USERID=$(id -u)
+LOGS_FOLDER="var/logs/shell-scripts"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID -ne 0 ] ; then
+echo "plase run this command with Root user"
+exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ] ; then
+    echo "$2...installation has failed" |tee -a $LOGS_FILE
+    else 
+    echo "$2...installation is successful" |tee -a $LOGS_FILE
+    fi
+
+    }
+
+    for package in #@
+    do
+    dnf install $package -y & >> $LOGS_FILE
+ VALIDATE $? "$package"
+    done
+
+    
