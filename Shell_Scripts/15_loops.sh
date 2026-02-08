a6b153e29d40196b8e7a444ce3eff093fc295308
Satish Narayana <satishnarayana07@gmail.com>
2026-02-08 20:07:51 +0530

    practice

--- Shell_Scripts/15_loops.sh
+++ Shell_Scripts/15_loops.sh
@@ -3,11 +3,13 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell_logs/"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 
+#1.Root use check
 if [ $USERID -ne 0 ] ; then
-echo "please run this command with Root User"
+echo "ERROR: please run this command with Root User ..like using 'sudo'"
 exit 1
 fi
 
+#2.Creae Log Directory
 mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
@@ -18,16 +20,16 @@ echo "$2 is successfully installed"  | tee -a $LOGS_FILE
 fi
 }
 
-
-for package in $@ 
+#loop through arguments
+for package in "$@ "
 do
-dnf list installed $package &>>$LOGS_FILE
+dnf list installed "$package" &>>$LOGS_FILE
 if [ $? -ne 0 ] ; then
 echo "$package not installed..installing now" 
-dnf install $package -y & >> $LOGS_FILE
-VALIDATE $? "$package"
+dnf install $package -y &>>$LOGS_FILE
+VALIDATE $? "installation of $package"
 else
-echo "$package is already installed"
+echo "$package is already installed" | tee -a $LOGS_FILE
 fi
 done 
 
