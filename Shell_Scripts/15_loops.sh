756c061a76506f2634db917c3869575e46b1c2c1
Satish Narayana <satishnarayana07@gmail.com>
2026-02-08 22:44:08 +0530

    practice

--- Shell_Scripts/15_loops.sh
+++ Shell_Scripts/15_loops.sh
@@ -25,7 +25,7 @@ for package in "$@"
 do
 dnf list installed "$package" &>>$LOGS_FILE
 if [ $? -ne 0 ] ; then
-echo "$package not installed..installing now" 
+echo  "$package not installed..installing now" 
 dnf install $package -y &>>$LOGS_FILE
 VALIDATE $? "installation of $package"
 else
---
+++ Shell_Scripts/17_colours.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/colour_logs/"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m" 
+
+if [ $USERID -ne 0 ] ; then
+echo -e " $R ERROR: $Y please run as Root user..like using $G 'sudo' $N"
+exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+if [ $1 -ne 0 ] ; then
+echo -e " $2 $R not installed " |tee -a $LOGS_FILE
+else
+echo -e "$2 is $G successfully installed"  | tee -a $LOGS_FILE
+fi
+}
+
+
+for package in $@
+do
+dnf list intalled "$package" &>>$LOGS_FILE
+if [ $? -ne 0 ] ; then
+echo -e " $Y $package $R not installed..$B installing now buddy"
+dnf install $package -y &>>$LOGS_FILE
+VALIDATE $? $package 
+else
+echo -e "Required software $package has already $G available"
+fi
+done
+
+
