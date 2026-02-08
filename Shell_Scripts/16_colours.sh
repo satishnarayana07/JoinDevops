c3e736eb12dbc1ee775cb54ac862bf2ff319e3e4
Satish Narayana <satishnarayana07@gmail.com>
2026-02-08 21:57:54 +0530

    practice

---
+++ Shell_Scripts/16_colours.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+
+# Reset color
+N="\e[0m"
+
+echo -e "$R Hello world $N, $Y I am learing linux $N"
