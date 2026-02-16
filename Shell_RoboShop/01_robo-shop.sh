f1bbde8c0c0a110f4a048e2787f1d8ad35ba71ee
Satish Narayana <satisnarayana07@gmail.com>
2026-02-16 17:38:34 +0530

    new Shell Robo Shop folder added

---
+++ Shell_RoboShop/01_robo-shop.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+AMI_ID=ami-0220d79f3f480ecf5
+SG_ID=sg-020b767a34adc9870
+ZONE_ID=Z07294741SXJ6JLY3QHAB
+DOMAIN_NAME=sandarshantv.online
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m" 
+N="\e[0m"
+
+for instance in $@
+do
+INSTANCE_ID=$(aws ec2 run-instances 
+--image-id $AMI_ID 
+--instance-type t3.micro
+--security-group-ids $SG_ID
+-tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" 
+--query 'Instances[0].InstanceId' 
+--output text)
+
+if [ $instance != "frontend" ]; then
+IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+RECORD_NAME="$instance.$DOMAIN_NAME"
+else
+IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+RECORD_NAME="$instance.$DOMAIN_NAME"
+fi
+echo "$instance:$IP"
+
+ aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  ' 
+  done
+
