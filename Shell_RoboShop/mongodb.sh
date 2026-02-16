d60219816cabefe5dc47a73d786b0d750f801a20
Satish Narayana <satisnarayana07@gmail.com>
2026-02-16 22:15:33 +0530

    new changes saved

--- Shell_RoboShop/mongodb.sh
+++ Shell_RoboShop/mongodb.sh
@@ -4,20 +4,16 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/logs/"
 SCRIPT_NAME=$(echo $0|cut -d "." -f1)
 LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 B="\e[34m" 
 N="\e[0m"
-
 if [ $USERID -ne 0 ] ; then
 echo -e " $R ERROR: $Y please run as Root user..like using $G 'sudo' $N"
 exit 1
 fi
-
 mkdir -p $LOGS_FOLDER
-
 VALIDATE(){
 if [ $1 -ne 0 ] ; then
 echo -e " $2 $R not installed " |tee -a $LOGS_FILE
@@ -25,9 +21,7 @@ else
 echo -e "$2 is $G successfully installed"  | tee -a $LOGS_FILE
 fi
 }
-
 ##### Mongo DB #####
-
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGS_FILE
 VALIDATE $? "copying mongo.repo"
 
