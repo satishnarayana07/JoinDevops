6feda38bc40ee02a432038532cfcb11d4b7e2cea
Satish Narayana <satisnarayana07@gmail.com>
2026-02-16 18:19:06 +0530

    new changes saved

--- Shell_RoboShop/01_robo-shop.sh
+++ Shell_RoboShop/01_robo-shop.sh
@@ -11,14 +11,7 @@ N="\e[0m"
 
 for instance in $@
 do
-INSTANCE_ID=$(aws ec2 run-instances 
---image-id $AMI_ID 
---instance-type t3.micro
---security-group-ids $SG_ID
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" 
---query 'Instances[0].InstanceId' 
---output text)
-
+INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 if [ $instance != "frontend" ]; then
 IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 RECORD_NAME="$instance.$DOMAIN_NAME"
